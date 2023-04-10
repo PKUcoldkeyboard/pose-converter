654dfc2d79bfd95939f2634585652dc1e8424a74
cuterwrite <158505862@qq.com>
2023-04-10 20:47:39 +0800

    init backend project

---
+++ src/main/java/llm/poseconverter/PoseConverterApplication.java
@@ -0,0 +1,13 @@
+package llm.poseconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoseConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoseConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/llm/poseconverter/PoseConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package llm.poseconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoseConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
